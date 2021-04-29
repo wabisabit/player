0959d5d5896a11946d3c5f738495e1707c608ea4
Roman Ljubimov <romanrandom@gmail.com>
2021-04-29 13:40:22 +0200

    Remove unused Song parameters

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,7 @@ export type Song = {
   id: string
   name: string
   cover_image_path: string
-  cover_image_aspect_ratio: string
   music_file_path: string
-  music_file_mimetype: string
 }
 
 export enum PlaybackState {
