51e2aac74e5dd070054245366c015756535130d8
Roman Ljubimov <romanrandom@gmail.com>
2021-04-29 09:57:03 +0200

    Add heartbeat animation to like button

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import heart from '../assets/heart.svg'
 import { likeSong } from '../services'
 
 const ICON_SIZE = 30
+const HEART_BEAT_RATIO = 1.1
 
 type Props = {
   id: string
@@ -25,12 +26,46 @@ function LikeButton(props: Props) {
   return <Icon src={heart} onClick={handleLikeClick} $isActive={isLiked} />
 }
 
+const heartBeatAnimation = keyframes`
+  0% {
+    transform: scale(${HEART_BEAT_RATIO}) translateY(0)
+  }
+  10% {
+    transform: scale(1) translateY(-2px)
+  }
+  20% {
+    transform: scale(${HEART_BEAT_RATIO}) 
+  }
+  30% {
+    transform: scale(1)
+  }
+  40% {
+    transform: scale(${HEART_BEAT_RATIO}) translateY(-1px)
+  }
+  100% {
+    transform: scale(1) translateY(0)
+  }
+`
+
 const Icon = styled.img<{ $isActive: boolean }>`
   width: ${ICON_SIZE}px;
   height: ${ICON_SIZE}px;
   opacity: ${(props) => (props.$isActive ? 1 : 0.5)};
   padding: 10px;
   cursor: pointer;
+  &:hover {
+    opacity: ${(props) => (props.$isActive ? 0.9 : 0.6)};
+    transform: scale(${(props) => (props.$isActive ? 1 : HEART_BEAT_RATIO)});
+  }
+  transition: all 0.2s ease-in;
+  ${(props) =>
+    props.$isActive &&
+    css`
+      animation-name: ${heartBeatAnimation};
+      animation-duration: 1s;
+      animation-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
+      animation-iteration-count: 1;
+    `}
 `
 
 export default LikeButton
--- src/components/Track.style.ts
+++ src/components/Track.style.ts
@@ -4,4 +4,7 @@ export const TrackContainer = styled.div`
   display: flex;
   justify-content: center;
   border-bottom: 1px solid #15181e;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.05);
+  }
 `
